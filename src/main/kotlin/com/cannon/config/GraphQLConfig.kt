a4c33d4eac7006291113b57a756a490331eb0efe
LeonZhou <zhouleib1412@gmail.com>
2019-09-06 17:06:48 +1200

    Graphql test (#7)
    * graphql test

--- src/main/kotlin/com/cannon/config/GraphQLConfig.kt
+++ src/main/kotlin/com/cannon/config/GraphQLConfig.kt
@@ -1,13 +1,2 @@
 package com.cannon.config
 
-import com.cannon.graphql.GraphQLExecutor
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class GraphQLConfig {
-    @Bean
-    fun graphQLExecutor(): GraphQLExecutor {
-        return GraphQLExecutor()
-    }
-}
