e23b3dca8efe17c9e9f042a007708154395a6f9a
leon.zhou <leon.zhou@vendhq.com>
2019-09-07 00:48:19 +1200

    GraphParser parse string to entity graph

--- src/main/kotlin/com/cannon/bean/Branch.kt
+++ src/main/kotlin/com/cannon/bean/Branch.kt
@@ -1,13 +1,14 @@
 package com.cannon.bean
 
-import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.OneToMany
+import javax.persistence.*
 
 @Entity
 class Branch(
         var name: String,
         var number: String,
+        @ManyToOne(cascade = [(CascadeType.REFRESH), (CascadeType.REMOVE)], optional = true)
+        @JoinColumn(name = "parent_id")
+        val parent: Branch? = null,
         @OneToMany(mappedBy = "branch", fetch = FetchType.LAZY)
         var users: MutableList<User>
 ) : BaseEntity()
