eba1953d36b7e7db8ff41cd708793bcc5a8724ec
Leon Zhou <zhouleib1412@gmail.com>
2020-06-07 19:25:20 +1200

    upgrade to 8

--- kotlin-cannon-api/build.gradle.kts
+++ kotlin-cannon-api/build.gradle.kts
@@ -92,7 +92,7 @@ fun DependencyHandlerScope.graphql() {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "1。8"
     }
 }
 
