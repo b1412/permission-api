46ff2a310a532b74e592c34c3febaaa1a01534d3
Leon Zhou <zhouleib1412@gmail.com>
2020-09-06 17:00:32 +1200

    update

--- kotlin-cannon-generator/src/main/kotlin/com/github/b1412/template/tasks/kotlin/ServiceTask.kt
+++ kotlin-cannon-generator/src/main/kotlin/com/github/b1412/template/tasks/kotlin/ServiceTask.kt
@@ -5,8 +5,8 @@ import com.github.b1412.template.TaskConstants
 
 
 class ServiceTask : MultipleTask(
-        replaceFile = false,
-        folder = { project, _ -> TaskConstants.apiPath + TaskConstants.srcPath + project.packageName.replace(".", "/") + "/" + "service" },
-        filename = { _, entity -> entity!!.name + "Service.kt" },
-        templatePath = "kotlin/service.ftl"
+    replaceFile = false,
+    folder = { project, _ -> TaskConstants.apiPath + TaskConstants.srcPath + project.packageName.replace(".", "/") + "/" + "service" },
+    filename = { _, entity -> entity!!.name + "Service.kt" },
+    templatePath = "kotlin/service.ftl"
 )
