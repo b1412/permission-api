a097d0ff1710e8b75f6dfafc66464e157fc43eee
leon.zhou <zhouleib1412@gmail.com>
2020-11-17 23:52:30 +1300

    update object mapper

--- permission-api/src/main/kotlin/com/github/b1412/permission/config/WebConfig.kt
+++ permission-api/src/main/kotlin/com/github/b1412/permission/config/WebConfig.kt
@@ -1,9 +1,19 @@
 package com.github.b1412.permission.config
 
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.github.b1412.json.JsonReturnHandler
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -14,5 +24,23 @@ class WebConfig(
 ) : WebMvcConfigurer {
     override fun addReturnValueHandlers(returnValueHandlers: MutableList<HandlerMethodReturnValueHandler>) {
         returnValueHandlers.add(jsonReturnHandler)
+    }
+
+    @Bean
+    fun mappingJackson2HttpMessageConverter(): MappingJackson2HttpMessageConverter {
+        val jsonConverter = MappingJackson2HttpMessageConverter()
+        val objectMapper = ObjectMapper()
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        objectMapper.registerModule(Jdk8Module())
+        objectMapper.registerModule(JavaTimeModule())
+        objectMapper.registerModule(Hibernate5Module().configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true))
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        jsonConverter.objectMapper = objectMapper
+        return jsonConverter
+    }
+
+    override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
+        converters.add(mappingJackson2HttpMessageConverter())
     }
 }
