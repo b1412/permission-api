00aa996becea6b242e7baeee967aeb4ee4471132
leon.zhou <zhouleib1412@gmail.com>
2020-11-19 17:46:37 +1300

    0.13

--- permission-api/build.gradle.kts
+++ permission-api/build.gradle.kts
@@ -23,7 +23,7 @@ noArg{
 }
 
 group = "com.github.b1412"
-version = "0.1.12"
+version = "0.1.13"
 
 val jar: Jar by tasks
 val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
--- permission-generated/build.gradle.kts
+++ permission-generated/build.gradle.kts
@@ -27,7 +27,7 @@ allOpen {
 }
 
 group = "com.github.b1412"
-version = "0.1.12"
+version = "0.1.13"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 
